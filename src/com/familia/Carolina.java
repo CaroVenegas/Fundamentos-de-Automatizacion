debd237b71512d559f844d4d76dde41c70d625c8
caro <carolina.benavides.venegas@ucreativa.com>
2021-06-19 19:40:27 -0600

    prueba

---
+++ src/com/familia/Carolina.java
@@ -0,0 +1,14 @@
+package com.ucreativa.familia;
+
+public class Carolina extends Maribel {
+    private  String saludo;
+    private int edad;
+    private String hobby;
+
+
+    public Carolina(String saludo) {
+        super(saludo);
+    }
+}
+
+
